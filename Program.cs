2a62da186e18276b574912d51ca7d6903bc83776
Иван Мамонов <mamonov_vanya03@mail.ru>
2021-06-28 09:51:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordsLine = Console.ReadLine();
+            var sb = new StringBuilder();
+
+            foreach (char c in wordsLine)
+            {
+                if (!char.IsPunctuation(c))
+                    sb.Append(c);
+            }
+
+            wordsLine = sb.ToString();
+
+            string[] wordsArr = wordsLine.Split(' ');
+
+            foreach (var w in wordsArr)
+            {
+                if (w[0] == w[w.Length-1])
+                {
+                    Console.WriteLine(w);
+                }
+            }
+        }
+    }
+}
